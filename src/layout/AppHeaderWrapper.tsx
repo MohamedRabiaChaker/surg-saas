d1d17f68e26c0e90bbd10405e1872265b99bdc93
mohamed.chaker <mohamed.chaker@ataccama.com>
2025-09-21 11:43:32 +0200

    remove redundant file

--- src/layout/AppHeaderWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import { getCurrentUser } from "@/lib/auth";
-import AppHeaderClient from "./AppHeaderClient";
-
-export default async function AppHeader() {
-  const user = await getCurrentUser();
-  
-  return <AppHeaderClient user={user} />;
-}
